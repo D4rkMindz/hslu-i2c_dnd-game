13683e0987740543a84f745bdaed4e3e5f3ec3c1
Megan <codebymegan@gmail.com>
2024-11-09 13:31:04 +0100

    Delete build directory

--- build/CMakeFiles/first.dir/main.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/first.dir/main.c.obj: \
- C:\Users\Andrea\OneDrive\ -\ Hochschule\ Luzern\Desktop\UNI\3\ADS\Practice_and_Exercises\C\hslu-i2c_dnd-game\main.c \
- C:\Users\Andrea\OneDrive\ -\ Hochschule\ Luzern\Desktop\UNI\3\ADS\Practice_and_Exercises\C\hslu-i2c_dnd-game\game.h
