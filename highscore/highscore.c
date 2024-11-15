bcf220989bbb2b68359076955f1117352a7d91de
socalhenry <149694467+socalhenry@users.noreply.github.com>
2024-11-15 16:24:35 +0100

    Add files via upload

---
+++ highscore/highscore.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void updateHighScore(Character* player) {
+    FILE* file;
+    int highScore = 0;
+
+    // Open the file for reading
+    file = fopen("highscore.txt", "r");
+    if (file != NULL) {
+        // Read the high score from the file
+        fscanf(file, "%d", &highScore);
+        fclose(file);
+    }
+    // Shows the new high score
+    if (player->pointsValue > highScore) {
+        // Open the file for writing and update the high score
+        file = fopen("highscore.txt", "w");
+        if (file == NULL) {
+            perror("Error opening file for writing");
+            return;
+        }
+        fprintf(file, "%d", player->pointsValue);
+        fclose(file);
+
+        printf("Congratulations you've reached a new High Score: %d\n", highScore);
+    
+    // Shows the current high score
+    } else {
+        printf("High Score remains: %d\n", highScore);
+        printf("Your Current Score: %d\n", player->pointsValue);
+    }
+}
