311e61731e3f0b3347e06440d838d371895f8601
D4rkMindz <bjoern.pfoster@gmail.com>
2024-11-18 10:51:11 +0100

    Optimized character creation using fancy_print and safe-guards
    Signed-off-by: D4rkMindz <bjoern.pfoster@gmail.com>

--- character/character.c
+++ character/character.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include "character.h"
+#include <ctype.h>
 #include "../utils.h"
-
 #include <string.h>
 
-Character getEnemy(char name[MAX_NAME_LEN], int health, int attackPower, int defense, char main_weapon[MAX_WEAPON_NAME_LEN], int healthReturn, int pointValue);
+Character getEnemy(
+    char name[MAX_NAME_LEN],
+    int health,
+    int attackPower,
+    int defense,
+    char main_weapon[MAX_WEAPON_NAME_LEN],
+    int healthReturn,
+    int pointValue
+);
 
 static Character player;
 
 void createCharacter() {
     char class[10];
-    fancy_print("Select your characters class [barbarian,wizard]:");
-    scanf("%s", class);
+    while (!equals(class, "barbarian") && !equals(class, "wizard")) {
+        fancy_print("Select your characters class [barbarian,wizard]:");
+        scanf("%s", class);
+    }
     fancy_print("Enter your character's name:");
     scanf("%s", player.name);
-    int health = 100;
-    int attackPower = 10;
-    int defense = 5;
+    int health = CHARACTER_HEALTH;
+    int attackPower = BARBARIAN_ATTACK;
+    int defense = BARBARIAN_DEFENSE;
     char playerWeapon[25];
-    
+
     if (equals(class, "barbarian")) {
         health = CHARACTER_HEALTH;
         attackPower = BARBARIAN_ATTACK;
@@ -37,34 +47,66 @@ void createCharacter() {
     strcpy(player.main_weapon, playerWeapon);
     player.healthReturn = CHARACTER_HEALTH_RETURN;
     player.pointValue = CHARACTER_POINT_VALUE;
-    printf("Character %s created with %d health.\n", player.name, player.health);
+    fancy_print("Character %s created with %d health.\n", player.name, player.health);
 }
 
 Character getSlime() {
-    return getEnemy("Slime", SLIME_HEALTH, SLIME_ATTACK, SLIME_DEFENSE, SLIME_WEAPON, SLIME_HEALTH_RETURN, SLIME_POINT_VALUE);
+    return getEnemy(
+        "Slime",
+        SLIME_HEALTH,
+        SLIME_ATTACK,
+        SLIME_DEFENSE,
+        SLIME_WEAPON,
+        SLIME_HEALTH_RETURN,
+        SLIME_POINT_VALUE
+    );
 }
 
 Character getSkeleton() {
-    return getEnemy("Skeleton", SKELETON_HEALTH, SKELETON_ATTACK, SKELETON_DEFENSE, SKELETON_WEAPON, SKELETON_HEALTH_RETURN, SKELETON_POINT_VALUE);
+    return getEnemy(
+        "Skeleton",
+        SKELETON_HEALTH,
+        SKELETON_ATTACK,
+        SKELETON_DEFENSE,
+        SKELETON_WEAPON,
+        SKELETON_HEALTH_RETURN,
+        SKELETON_POINT_VALUE
+    );
 }
 
 Character getKingSkeleton() {
-    return getEnemy("Skeleton King", ENDBOSS_HEALTH, ENDBOSS_ATTACK, ENDBOSS_DEFENSE, ENDBOSS_WEAPON, ENDBOSS_HEALTH_RETURN, ENDBOSS_POINT_VALUE);
+    return getEnemy(
+        "Skeleton King",
+        ENDBOSS_HEALTH,
+        ENDBOSS_ATTACK,
+        ENDBOSS_DEFENSE,
+        ENDBOSS_WEAPON,
+        ENDBOSS_HEALTH_RETURN,
+        ENDBOSS_POINT_VALUE
+    );
 }
 
 Character *getPlayerCharacter() {
     return &player;
 }
 
-Character getEnemy(char name[MAX_NAME_LEN], const int health, const int attackPower, const int defense, char main_weapon[MAX_WEAPON_NAME_LEN], const int healthReturn, const int pointValue) {
+Character getEnemy(
+    char name[MAX_NAME_LEN],
+    const int health,
+    const int attackPower,
+    const int defense,
+    char main_weapon[MAX_WEAPON_NAME_LEN],
+    const int healthReturn,
+    const int pointValue
+) {
     Character enemy;
     strcpy(enemy.name, name);
     enemy.health = health;
     enemy.attackPower = attackPower;
     enemy.defense = defense;
-    strcpy(enemy.main_weapon, main_weapon[]);
+    strcpy(enemy.main_weapon, main_weapon);
     enemy.healthReturn = healthReturn;
     enemy.pointValue = pointValue;
-    
+
     return enemy;
 }
