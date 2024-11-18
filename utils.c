4e5b762e7512fde7160ae6d3c39cba1cde9481a0
D4rkMindz <bjoern.pfoster@gmail.com>
2024-11-18 10:50:01 +0100

    Optimized fancy_print to work exactly the same as printf with multiple parameters
    Signed-off-by: D4rkMindz <bjoern.pfoster@gmail.com>

--- utils.c
+++ utils.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 char *to_lower_string(char *str) {
     for (char *p = str; *p; p++) *p = tolower(*p);
@@ -13,15 +14,24 @@ _Bool equals(char *command, char *check) {
     return strcmp(command, check) == 0;
 }
 
-void fancy_print(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        printf("%c", str[i]);
+void fancy_print(const char *str, ...) {
+    char buffer[1024]; // Buffer to hold the formatted string.
+    va_list args;
+
+    va_start(args, str);
+    vsnprintf(buffer, sizeof(buffer), str, args); // Format the string safely.
+    va_end(args);
+
+    const char *text = buffer;
+
+
+    while (*text != '\0') {
+        putchar(*text);
         fflush(stdout);
-        usleep(50*1000);
-        i++;
-        if (str[i] == '\n') {
-            while (getchar() != '\n') continue;
+        if (*text == '\n') {
+            while (getchar() != '\n');
         }
+        usleep(10 * 1000); // 10k nanoseconds = 10 miliseconds
+        text++;
     }
 }
--- utils.h
+++ utils.h
@@ -2,5 +2,5 @@
 #define UTILS_H
 char *to_lower_string(char *str);
 _Bool equals(char *command, char *check);
-void fancy_print(char *str);
+void fancy_print(char *str, ...);
 #endif // UTILS_H
